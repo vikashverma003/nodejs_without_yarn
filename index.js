e08d8818a1b864c6c1c78bd359d90ee502b1aab2
vikashverma8960 <vikashverma1405@gmail.com>
2021-12-18 16:46:37 +0530

    first commit

--- index.js
+++ index.js
@@ -30,6 +30,6 @@ app.use("/api/auth", authRoute);
 app.use("/api/users", userRoute);
 app.use("/api/posts", postRoute);
 
-app.listen(8800, () => {
+app.listen(process.env.PORT, () => {
   console.log("Backend server is running!");
 });
